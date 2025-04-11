c55f567debc482881c41650aed6b544cd4e7677b
Pedro <pedrorodl07@gmail.com>
2025-04-11 18:00:54 -0300

    agora está tudo funcionando corretamente

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
   get "dashboard" => "dashboard#show", as: :dashboard
-  delete "logout" => "devise/sessions#destroy", as: :destroy_user_session_path
 end
